aba7bbe7b8fff574340aa475dd39830fa4ad38f9
Pier Carlo Cadoppi <piercarlo.cadoppi@mail.polimi.it>
2020-04-29 01:09:36 +0200

    ok5

---
+++ build/precache-manifest.6ff2809b7b4fb080e62c84a28012db20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1221d5653086c73eee9ec3f1ebe4b52f",
+    "url": "/basic-apollo-app/index.html"
+  },
+  {
+    "revision": "fceacc92bd51ddf6a6bb",
+    "url": "/basic-apollo-app/static/js/2.4ec6723f.chunk.js"
+  },
+  {
+    "revision": "22426e88834b60a497e43b7be5c04e33",
+    "url": "/basic-apollo-app/static/js/2.4ec6723f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a3dfeb9473066cd494a",
+    "url": "/basic-apollo-app/static/js/main.fc737e13.chunk.js"
+  },
+  {
+    "revision": "b67d9af85b3d3ec1bffe",
+    "url": "/basic-apollo-app/static/js/runtime-main.94a2a99a.js"
+  }
+]);
